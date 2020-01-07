5c2eb47607dff232183c289eca71bf56b853644d
Gervis Bermudez <gbermudezmora@quaresitsolutions.com>
2020-01-07 15:37:09 -0300

    Adding Vue UI and Niko blog template

---
+++ resources/js/components/Blog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-md-4 d-flex ftco-animate fadeInUp ftco-animated">
+    <div class="blog-entry justify-content-end">
+      <a href="single.html" class="block-20" style="background-image: url('images/image_1.jpg');"></a>
+      <div class="text mt-3 float-right d-block">
+        <div class="d-flex align-items-center mb-3 meta">
+          <p class="mb-0">
+            <span class="mr-2">March 23, 2019</span>
+            <a href="#" class="mr-2">Admin</a>
+            <a href="#" class="meta-chat">
+              <span class="icon-chat"></span> 3
+            </a>
+          </p>
+        </div>
+        <h3 class="heading">
+          <a href="single.html">
+            Why Lead Generation is Key for Business
+            Growth
+          </a>
+        </h3>
+        <p>
+          A small river named Duden flows by their place and supplies it with the necessary
+          regelialia.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
